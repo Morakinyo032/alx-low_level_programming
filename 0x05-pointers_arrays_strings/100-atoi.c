542e733bd236bd220f7ae05e0090db9752504dbc
Morakinyo032 <ibraheemabdulwakeel55@gmail.com>
2022-12-20 23:43:17 +0100

    Files correction

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,35 @@
  * @s: input string
  * Return: 0
  */
+int digitchk(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (-1);
+}
 int _atoi(char *s)
 {
-	return (atoi(s));
+	int sign, num, i;
+	sign = 1;
+	num = 0;
+	if (!s)
+		return (0);
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (digitchk(s[i]) == -1)
+		{
+			if (s[i] == '-')
+				sign *= -1;
+			continue;
+		}
+		num += (s[i] - '0');
+		if (digitchk(s[i + 1]) != 1)
+			break;
+		num *= 10;
+	}
+	if (sign != 1)
+		return (-num);
+	return (num);
 }
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,21 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 /**
- * rev_string - prints string in reverse
- * @s: string input
+ * rev_string - Reverses a string
+ * @s: Pointer to the string to be reversed
+ * Return: Nothing
  */
 void rev_string(char *s)
 {
-	strrev(s);
+	int i, j, len;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	len = i;
+	char t[len];
+	for (j = 0; j < i; j++)
+		t[j] = s[i -j -1];
+	t[j] = '\0';
+	for (j = 0; j < i; j++)
+		s[j] = t[j];
 }
