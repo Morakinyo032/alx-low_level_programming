533a1a369e984090f6efe8b21ac7e685b74340e3
Morakinyo032 <ibraheemabdulwakeel55@gmail.com>
2022-12-29 22:49:09 +0100

    Fixing programs

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,21 +1,6 @@
 #include "main.h"
 #include <string.h>
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: Pointer to string to check
- * Return: 1, if true and 0 otherwise
- */
-int pal(char *d, int a, int b);
-int is_palindrome(char *s)
-{
-	int len;
-
-	len = strlen(s);
-	if (len == 0)
-		return (1);
-	return (pal(s, 0, len - 1));
-}
-/**
  * pal - checks if a string is a palindrome
  * @d: Pointer to string to check
  * @a: int input
@@ -34,3 +19,20 @@ int pal(char *d, int a, int b)
 		return (0);
 	return (1);
 }
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: Pointer to string to check
+ * Return: 1, if true and 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	if (len == 0)
+		return (1);
+	return (pal(s, 0, len - 1));
+}
+
+
