c97c9873e378eaae5e070e37d7caa7a22241ea48
Morakinyo032 <ibraheemabdulwakeel55@gmail.com>
2023-01-16 11:04:07 +0100

    Fixing programs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - Creates a new dog
  * @name: Pointer to dog's name
@@ -11,11 +12,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *s;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
 	s = malloc(sizeof(dog_t));
 	if (s == NULL)
 		return (NULL);
-	s->name = name;
+	s->name = strdup(name);
+	if (s->name == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
 	s->age = age;
-	s->owner = owner;
+	s->owner = strdup(owner);
+	if (s->owner == NULL)
+	{
+		free(s->name);
+		free(s);
+		return (NULL);
+	}
 	return (s);
 }
---
+++ 0x0E-structures_typedef/memcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _memcpy - Copies the content of a memory space to another
+ * @src: Pointer to the memory to be copied
+ * @des: Pointer to where to copy to
+ * @size: size of bytes to copy
+ * Return: des
+ */
+void *_memcpy(void *src, void *des, size_t size)
+{
+	des = malloc(size);
+	if des
+}
+
