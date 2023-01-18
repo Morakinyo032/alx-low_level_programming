8144b3fc7d03194bcdd75bd7712e920f14cb63cd
Morakinyo032 <ibraheemabdulwakeel55@gmail.com>
2023-01-18 14:28:38 +0100

    Fixing programs

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,9 +12,16 @@
  */
 void print_sep(const char * const s, int idx)
 {
-	if (s[idx + 1] != '\0')
-		printf(", ");
+	switch (s[idx + 1])
+	{
+		case '\0':
+			break;
+		default:
+			printf(", ");
+			break;
+	}
 }
+
 /**
  * print_all - Prints anything
  * @format: List of all types of arg passed
@@ -28,37 +35,40 @@ void print_all(const char * const format, ...)
 
 	i = 0;
 	va_start(args, format);
-	while (format[i] != '\0')
+	if (format != NULL)
 	{
-		switch (format[i])
+		while (format[i] != '\0')
 		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				print_sep(format, i);
-				break;
-			case 'i':
-				printf("%i", va_arg(args, int));
-				print_sep(format, i);
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				print_sep(format, i);
-				break;
-			case 's':
-				tmp = va_arg(args, char *);
-				if (tmp == NULL)
-				{
-					printf("(nil)");
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c", va_arg(args, int));
 					print_sep(format, i);
 					break;
-				}
-				printf("%s", tmp);
-				print_sep(format, i);
-				break;
-			default:
-				break;
+				case 'i':
+					printf("%i", va_arg(args, int));
+					print_sep(format, i);
+					break;
+				case 'f':
+					printf("%f", va_arg(args, double));
+					print_sep(format, i);
+					break;
+				case 's':
+					tmp = va_arg(args, char *);
+					if (tmp == NULL)
+					{
+						printf("(nil)");
+						print_sep(format, i);
+						break;
+					}
+					printf("%s", tmp);
+					print_sep(format, i);
+					break;
+				default:
+					break;
+			}
+			i++;
 		}
-		i++;
 	}
 	va_end(args);
 	printf("\n");
