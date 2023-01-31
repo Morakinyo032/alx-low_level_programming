62914deec07efee2438306d953ac5906ca17c0c4
Morakinyo032 <ibraheemabdulwakeel55@gmail.com>
2023-01-31 10:05:10 +0100

    Fixing program

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -53,6 +53,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr1, *new;
 
+	if (*head == NULL)
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
--- 0x13-more_singly_linked_lists/9-main.c
+++ 0x13-more_singly_linked_lists/9-main.c
@@ -23,7 +23,7 @@ int main(void)
     add_nodeint_end(&head, 1024);
     print_listint(head);
     printf("-----------------\n");
-    insert_nodeint_at_index(&head, 5, 4096);
+    insert_nodeint_at_index(&head, 5, -4096);
     insert_nodeint_at_index(&head, 0, 4096);
     insert_nodeint_at_index(&head, 9, 4096);
     print_listint(head);
